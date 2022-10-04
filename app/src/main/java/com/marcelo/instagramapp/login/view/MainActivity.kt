ae288a43a12c01661126a4623d3c36c1078baefb
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-04 17:03:55 -0300

    iniciando

---
+++ app/src/main/java/com/marcelo/instagramapp/login/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.marcelo.instagramapp.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marcelo.instagramapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
