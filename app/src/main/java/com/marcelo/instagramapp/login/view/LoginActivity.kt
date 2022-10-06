98eea3cc0d8f231ffb939a83c6e7626485ae19d8
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-06 20:50:53 -0300

    TextWatcher

--- app/src/main/java/com/marcelo/instagramapp/login/view/LoginActivity.kt
+++ app/src/main/java/com/marcelo/instagramapp/login/view/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.marcelo.instagramapp.login.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.marcelo.instagramapp.databinding.ActivityLoginBinding
 
@@ -13,5 +15,30 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val editEmail = binding.loginEditEmail
+        val editPassword = binding.loginEditPassword
+
+        editEmail.addTextChangedListener(watcher)
+        editPassword.addTextChangedListener(watcher)
+
+        binding.loginBtnEntrar.setOnClickListener {
+            editEmail.error = "Esse e-mail é inválido"
+            editPassword.error = "Essa senha é inválida"
+        }
+    }
+
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            binding.loginBtnEntrar.isEnabled = s.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
     }
 }
