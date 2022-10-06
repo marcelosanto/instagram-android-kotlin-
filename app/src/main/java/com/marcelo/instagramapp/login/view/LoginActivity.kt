f9aea4767495d346084323734aa7b1fe06fbb2ad
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-06 17:43:59 -0300

    viewBinding enabled

--- app/src/main/java/com/marcelo/instagramapp/login/view/LoginActivity.kt
+++ app/src/main/java/com/marcelo/instagramapp/login/view/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.marcelo.instagramapp.login.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.marcelo.instagramapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.marcelo.instagramapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
