edcb7c075f5052950adac7401241eefb5424a813
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-07 14:40:40 -0300

    criado o CustomDialog

--- app/src/main/java/com/marcelo/instagramapp/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/marcelo/instagramapp/register/view/RegisterPhotoFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.marcelo.instagramapp.R
+import com.marcelo.instagramapp.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
 
@@ -15,5 +17,25 @@ class RegisterPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+
+        customDialog.addButton(R.string.photo, R.string.gallery) {
+            when (it.id) {
+                R.string.photo -> {
+                    Toast.makeText(requireContext(), "Foto mano", Toast.LENGTH_SHORT).show()
+                }
+
+                R.string.gallery -> {
+                    Toast.makeText(requireContext(), "Galeria Irmao", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        customDialog.show()
     }
 }
