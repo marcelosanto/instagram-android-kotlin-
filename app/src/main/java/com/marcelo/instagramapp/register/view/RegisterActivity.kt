9ef4645aa0d2435f301f0ffb5e2599c8935575e4
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-07 07:50:05 -0300

    criado o fragmento do email

---
+++ app/src/main/java/com/marcelo/instagramapp/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.marcelo.instagramapp.register.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marcelo.instagramapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
