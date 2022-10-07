063a6c2901586e45f97a2f035e08c763997909ae
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-07 14:53:53 -0300

    criado o layout do BottomNavigator

---
+++ app/src/main/java/com/marcelo/instagramapp/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.marcelo.instagramapp.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marcelo.instagramapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
