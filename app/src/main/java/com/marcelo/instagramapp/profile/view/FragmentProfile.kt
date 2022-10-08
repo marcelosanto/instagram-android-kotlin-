84d00822eece54a8396560cb0749c4736788b7b2
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-08 16:29:56 -0300

    Tela de Perfil de Usuário

---
+++ app/src/main/java/com/marcelo/instagramapp/profile/view/FragmentProfile.kt
@@ -0,0 +1,19 @@
+package com.marcelo.instagramapp.profile.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.marcelo.instagramapp.R
+
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
