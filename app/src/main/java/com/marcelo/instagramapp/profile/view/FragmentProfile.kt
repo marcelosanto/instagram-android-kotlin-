5ff5013e5d62d7087d8c89623ff4cdc3b0e3c1b4
Marcelo Santos <rocha.mer21@gmail.com>
2022-10-08 17:49:43 -0300

    Animação de Scrolling com NestedScrollView e CoordinatorLayout

--- app/src/main/java/com/marcelo/instagramapp/profile/view/FragmentProfile.kt
+++ app/src/main/java/com/marcelo/instagramapp/profile/view/FragmentProfile.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.marcelo.instagramapp.R
 
 class FragmentProfile : Fragment() {
@@ -16,4 +19,36 @@ class FragmentProfile : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rv = view.findViewById<RecyclerView>(R.id.profile_rv)
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+
+    }
+}
+
+private class PostAdapter() : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        return PostViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_profile_grid, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.bind(R.drawable.ic_insta_add)
+    }
+
+    override fun getItemCount() = 30
+
+    inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(image: Int) {
+            itemView.findViewById<ImageView>(R.id.item_profile_img_grid).setImageResource(image)
+        }
+
+    }
+
 }
