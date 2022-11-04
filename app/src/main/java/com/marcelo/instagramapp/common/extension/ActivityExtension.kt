7f1fb0d2f8d1c410fb097f0326e80632c9cd1c16
Marcelo Santos <rocha.mer21@gmail.com>
2022-11-04 11:35:10 -0300

    refatorando e criando uma funcao de extensao para animação

--- app/src/main/java/com/marcelo/instagramapp/common/extension/ActivityExtension.kt
+++ app/src/main/java/com/marcelo/instagramapp/common/extension/ActivityExtension.kt
@@ -1,5 +1,7 @@
 package com.marcelo.instagramapp.common.extension
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -15,4 +17,12 @@ fun Activity.hideKeyboard() {
     }
 
     inm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Activity.animationEnd(callback: () -> Unit): AnimatorListenerAdapter {
+    return object : AnimatorListenerAdapter() {
+        override fun onAnimationEnd(animation: Animator) {
+            callback.invoke()
+        }
+    }
 }
--- app/src/main/java/com/marcelo/instagramapp/splash/view/SplashActivity.kt
+++ app/src/main/java/com/marcelo/instagramapp/splash/view/SplashActivity.kt
@@ -1,11 +1,10 @@
 package com.marcelo.instagramapp.splash.view
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.marcelo.instagramapp.common.base.DependencyInjector
+import com.marcelo.instagramapp.common.extension.animationEnd
 import com.marcelo.instagramapp.databinding.ActivitySplashBinding
 import com.marcelo.instagramapp.login.view.LoginActivity
 import com.marcelo.instagramapp.main.MainActivity
@@ -26,10 +25,8 @@ class SplashActivity : AppCompatActivity(), Splash.View {
         presenter = SplashPresenter(this, DependencyInjector.splashRepository())
 
         binding.splashImg.animate().apply {
-            setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    presenter.authenticated()
-                }
+            setListener(animationEnd {
+                presenter.authenticated()
             })
             duration = 1000
             alpha(1.0f)
@@ -41,14 +38,13 @@ class SplashActivity : AppCompatActivity(), Splash.View {
 
     override fun goToMainScreen() {
         binding.splashImg.animate().apply {
-            setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    val intent = Intent(baseContext, MainActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                }
+            setListener(animationEnd {
+                val intent = Intent(baseContext, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             })
+
             duration = 1000
             alpha(0.0f)
             start()
@@ -59,20 +55,16 @@ class SplashActivity : AppCompatActivity(), Splash.View {
 
     override fun goToLoginScreen() {
         binding.splashImg.animate().apply {
-            setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    val intent = Intent(baseContext, LoginActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                }
+            setListener(animationEnd {
+                val intent = Intent(baseContext, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             })
             duration = 1000
             alpha(0.0f)
             start()
         }
-
-
     }
 
     override fun onDestroy() {
